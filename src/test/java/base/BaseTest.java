21845f46c5f6a6aec51efe90318d9ca589f0a41d
Akanksha Gupta <akkigupta2604@gmail.com>
2026-06-19 16:37:51 +0530

    Initial Selenium Framework Setup

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,29 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class BaseTest {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        driver.quit();
+    }
+}
